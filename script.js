755cbb6f31c8ebfc0daf3dd5a63c5500ebd9e8c6
abinesh8900 <abineshranganathan@gmail.com>
2021-07-03 15:19:30 +0530

    modified hero

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const toggleMenu = document.querySelector("#toggle-menu");
 const navBar = document.querySelector(".hidden");
+const removeHeroContent = document.querySelector(".hero");
+const headerMobi = document.querySelector(".nav-holder");
 
 const openMenu = function () {
   navBar.classList.toggle("hidden");
   toggleMenu.classList.toggle("fa-bars");
   toggleMenu.classList.toggle("fa-times");
+  removeHeroContent.classList.toggle("hidden");
+  headerMobi.classList.toggle("header__mobi");
 };
 
 toggleMenu.addEventListener("click", openMenu);
